afe7c4f70a5d5ee0d133730c6b1996426a199afd
balu <balu5a212@gmail.com>
2021-02-14 14:52:08 +0530

    project to local repo

---
+++ src/test/java/com/app/second/SecondAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.second;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
